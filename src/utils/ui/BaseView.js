766440239dd73ad070e6aaa798b3af3451abd178
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-12-06 16:02:19 +0100

    fix(views): Handle non-serializable arguments in `BaseView` and store separately

--- src/utils/ui/BaseView.js
+++ src/utils/ui/BaseView.js
@@ -1,14 +1,35 @@
 export default class BaseView {
 	static #currentViewInstance = null;
+	static #nonSerializableArgs = new Map(); // Store non-serializable args separately
 
 	static async switchView(ViewClass, appContainer, appState, api, pushHistory = true, ...args) {
 		if (this.#currentViewInstance && typeof this.#currentViewInstance.destroy === 'function')
 			this.#currentViewInstance.destroy();
+		const serializableArgs = [];
+		const nonSerializableArgs = [];
+
+		args.forEach((arg, index) => {
+			try {
+				JSON.stringify(arg);
+				serializableArgs.push({ index, value: arg });
+			} catch {
+				nonSerializableArgs.push({ index, value: arg });
+			}
+		});
 
 		this.#currentViewInstance = new ViewClass(appContainer, appState, api, ...args);
 		appState.setCurrentView(this.#currentViewInstance);
-		if (pushHistory)
-			window.history.pushState({ view: ViewClass.name, args: args }, '');
+
+		if (pushHistory) {
+			const viewKey = ViewClass.name;
+			if (nonSerializableArgs.length > 0) {
+				this.#nonSerializableArgs.set(viewKey, nonSerializableArgs);
+			}
+			// Store only serializable args in history
+			const historyArgs = serializableArgs.map(a => a.value);
+			window.history.pushState({ view: ViewClass.name, args: historyArgs }, '');
+		}
+
 		if (typeof this.#currentViewInstance.render === 'function')
 			await this.#currentViewInstance.render();
 
@@ -17,6 +38,10 @@ export default class BaseView {
 
 	static getCurrentView() {
 		return this.#currentViewInstance;
+	}
+
+	static getNonSerializableArgs(viewName) {
+		return this.#nonSerializableArgs.get(viewName) || [];
 	}
 
 	constructor(app) {
