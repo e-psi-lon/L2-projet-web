eb2a0a1e49792bab0a0af3d6b8d086b242560ce5
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-12-06 22:38:11 +0100

    feat(vite): Add `@vitejs/plugin-basic-ssl` to enable HTTPS in dev environment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
+import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 import { resolve } from 'path'
 
 export default defineConfig(
 	{
 		base: '/',
 		plugins: [
-			tailwindcss()
+			tailwindcss(),
+			viteBasicSslPlugin({
+				name: 'test',
+				domains: ['localhost'],
+				certDir: resolve(__dirname, './certs')
+			})
+
 		],
 		resolve: {
 			alias: {
@@ -17,6 +24,13 @@ export default defineConfig(
 				'@views': resolve(__dirname, './src/views'),
 				'@ui': resolve(__dirname, './src/utils/ui'),
 				'@data': resolve(__dirname, './src/utils/data')
+			}
+		},
+
+		server: {
+			https: {
+				key: resolve(__dirname, './certs/localhost-key.pem'),
+				cert: resolve(__dirname, './certs/localhost.pem'),
 			}
 		}
 	}
