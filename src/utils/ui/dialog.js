2bf570a60b1d2b9266434bc173cca8be3678c165
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-12-09 13:20:29 +0100

    fix(dialog): Allow for scrolling on dialogs that are really too big

--- src/utils/ui/dialog.js
+++ src/utils/ui/dialog.js
@@ -35,14 +35,16 @@ export const displayDialog = async ({ DialogComponentOrContent, parent, onClose,
 				if (e.target === dialogElement) handleClose('backdrop');
 			}
 		},
-		div({ className: 'p-4 flex flex-col gap-4 h-full' },
-			div({ className: 'flex justify-end' },
+		div({ className: 'flex flex-col h-full' },
+			div({ className: 'flex justify-end flex-shrink-0 p-4 sticky top-0 bg-inherit' },
 				button({
 					className: 'bg-transparent border-0 p-0 m-0 outline-none cursor-pointer',
 					onClick: () => handleClose('x-button')
 				}, icon(X, { className: 'w-5 h-5 hover:text-red-500 transition-colors duration-200' }))
 			),
-			contentDiv
+			div({ className: 'overflow-y-auto flex-1 px-4 pb-4' },
+				contentDiv
+			)
 		)
 	);
 
